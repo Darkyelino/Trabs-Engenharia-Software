dd31c0863945585dddf21ca28c2741906627cf03
Douglas Moura <d.moura250304@gmail.com>
2025-09-10 00:15:19 -0500

    Fix: correção de bugs nas urls

--- webgenda/webgenda/urls.py
+++ webgenda/webgenda/urls.py
@@ -1,38 +1,42 @@
-"""
-URL configuration for webgenda project.
+# webgenda/urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.urls import path, include, reverse_lazy
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from agenda.views import register_view  # Sua view de registro customizada
 from django.conf import settings
 from django.conf.urls.static import static
-from agenda.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+
+    path('login/', auth_views.LoginView.as_view(template_name='agenda/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('registrar/', register_view, name='registrar'),
 
     path('perfil/mudar-senha/', auth_views.PasswordChangeView.as_view(
         template_name='agenda/password_change_form.html',
-        success_url='/perfil/mudar-senha/concluido/'
+        success_url=reverse_lazy('password_change_done')
     ), name='password_change'),
     path('perfil/mudar-senha/concluido/', auth_views.PasswordChangeDoneView.as_view(
         template_name='agenda/password_change_done.html'
     ), name='password_change_done'),
+
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='agenda/password_reset_form.html',
+        email_template_name='agenda/password_reset_email.html',
+        success_url=reverse_lazy('password_reset_done')
+    ), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='agenda/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='agenda/password_reset_confirm.html',
+        success_url=reverse_lazy('password_reset_complete')
+    ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='agenda/password_reset_complete.html'
+    ), name='password_reset_complete'),
 
     path('', include('agenda.urls')),
 ]
