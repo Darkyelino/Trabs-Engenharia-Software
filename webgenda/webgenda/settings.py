16ebefc120f0d16fef14d27eb2d72b9eb3bf8546
Douglas Moura <d.moura250304@gmail.com>
2025-09-16 09:58:46 -0500

    Tentativa de correção na visualização de comprovantes

--- webgenda/webgenda/settings.py
+++ webgenda/webgenda/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'agenda',
     'cloudinary_storage',
     'cloudinary',
+    'agenda',
 ]
 
 MIDDLEWARE = [
