f4fccb17c5b6d2676b73e06502833699095c67d8
Douglas Moura <d.moura250304@gmail.com>
2025-09-17 10:26:00 -0500

    Torna o email único

--- webgenda/agenda/models.py
+++ webgenda/agenda/models.py
@@ -5,6 +5,7 @@ from django.contrib.contenttypes.models import ContentType
 
 class Docentes(AbstractUser):
     nome = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.nome or self.username
